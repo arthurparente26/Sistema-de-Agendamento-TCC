9dcf425001d8b0125b8f9d9ba47411690d04599f
arthurparente26 <arthurparente26@gmail.com>
2022-06-24 08:35:19 -0300

    Atualização de listagem devido a mudanças

--- listar/listar_orcamentos.php
+++ listar/listar_orcamentos.php
@@ -60,7 +60,7 @@
   mysqli_close($conexao);
 
   ?>
-
+<!-- Modal de cadastro de orçamento -->
   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true" style="text-align: center;">
     <div class="modal-dialog" role="document">
       <div class="modal-content ">
@@ -76,6 +76,23 @@
       </div>
     </div>
   </div>
+
+  <!-- Modal de Exclusão de orçamento -->
+  <div class="modal fade" id="myModalExcluir" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true" style="text-align: center;">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content ">
+          <div class="modal-header alert alert-danger" style="height: 60px;">
+            <h4 class="modal-title " id="exampleModalLongTitle">Exclusão de Orçamento</h2>
+              <button type="button" class="close col-md-1" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">x</span>
+              </button>
+          </div>
+          <div class="modal-body">
+            <h5>Orçamento excluído com sucesso!</h5>
+          </div>
+        </div>
+      </div>
+    </div>
 
   <div class="container py-5">
     <table class="table table-striped table-light">
@@ -94,26 +111,6 @@
         <?php
         while ($user_data = mysqli_fetch_assoc($resultado)) {
 
-          echo "<!-- Modal -->
-            <div class='modal fade' id='modalExemplo' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-              <div class='modal-dialog' role='document'>
-                <div class='modal-content'>
-                  <div class='modal-header alert alert-danger'>
-                    <h5 class='modal-title' id='exampleModalLabel'>Exclusão de Orçamento</h5>
-                    <button type='button' class='close' data-dismiss='modal' aria-label='Fechar'>
-                      <span aria-hidden='true'>&times;</span>
-                    </button>
-                  </div>
-                  <div class='modal-body'>
-                    <p style='color: #f00;'>*Ao confirmar a exclusão, o orçamento selecionado será excluído permanentemente do sistema!</p>
-                  </div>
-                  <div class='modal-footer'>
-                    <button type='button' class='btn btn-secondary ' data-dismiss='modal'>Fechar</button>
-                    <a class = 'btn  btn-primary btn-danger' href = '../delete/delete_orcamento.php?id=$user_data[id]'>Excluir</a>
-                  </div>
-                </div>
-              </div>
-            </div>";
           echo "<tr>";
           echo "<td>" . $user_data['nomeC'] . "</td>";
           if ($user_data['tira_risco'] == 1) {
@@ -139,22 +136,9 @@
           echo "<td>" . date('d/m/Y', strtotime($user_data['dia'])) . "</td>";
           echo "<td>" . "R$ " . $user_data['valor'] . "</td>";
           echo "<td> <a class = 'btn btn-sm btn-primary' href = '../alter/alt_orcamento.php?id=$user_data[id]'><i class='bi bi-pencil-square'></i> </a> </td>";
-          echo "<td>
-            <button type='button' class='btn btn-sm btn btn-sm btn-danger' data-toggle='modal' data-target='#modalExemplo'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
-            <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>
-            <path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>
-          </svg>
-</button>
-           
-            </td>";
-          echo "<td>
-            <a class = 'btn btn-sm btn-info' href = '../gerar_pdf.php?id=$user_data[id]'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-filetype-pdf' viewBox='0 0 16 16'>
-            <path fill-rule='evenodd' d='M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z'/>
-          </svg>     
+          echo "<td> <a class = 'btn btn-sm btn-danger' href = '../delete/delete_orcamento.php?id=$user_data[id]'><i class='bi bi-trash''></i> </a> </td>";
+          echo "<td> <a class = 'btn btn-sm btn-info' href = '../gerar_pdf.php?id=$user_data[id]'><i class='bi bi-filetype-pdf''></i> </a> </td>";
             
-                </td>";
           echo "</tr>";
 
           if (!empty($_SESSION['sucesso'])) {
@@ -164,6 +148,14 @@
         </script>";
             unset($_SESSION['sucesso']);
           }
+        }
+
+        if (!empty($_SESSION['excluir'])) {
+          echo "<script type='text/javascript'>
+          $(window).on('load',function(){
+          $('#myModalExcluir').modal('show'); });
+      </script>";
+          unset($_SESSION['excluir']);
         }
         ?>
       </tbody>
